0ee6d6112d9a3cf272f37163d092dea8f5bb6818
Changguen Kim <rms1359@gmail.com>
2024-03-03 23:02:01 +0900

    know how to control manifest error for pwa download button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import { Button, Navbar, Container, Nav } from 'react-bootstrap'
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img src={process.env.PUBLIC_URL + '/logo192.png'} />
+      <img src={process.env.PUBLIC_URL + 'images/favicon.ico'} />
       하이aysasd
     </div>
   );
