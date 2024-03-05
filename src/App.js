d6a414e8d016a1709e81c4eff2cc7d1107894d99
Changguen Kim <rms1359@gmail.com>
2024-03-06 00:23:26 +0900

    choose how to make 2nd kwed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import styled from 'styled-components'
-
 import './App.css';
 
+import { data } from './components/data.js';
+
+
 function App() {
+
+  let navigate = useNavigate();
+
   return (
-    <div className="App">
-      <img src={process.env.PUBLIC_URL + 'images/favicon.ico'} />
-      하이aysasd
+
+    <div>
+      {data.map((item) => (
+        <div className='card' key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
+          <img className='imgsize' src={item.img} alt={item.title} />
+        </div>
+      ))}
     </div>
+
   );
 }
 
---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+const img1 = `${process.env.PUBLIC_URL}/images/bg.jpg`;
+
+
+
+export const data = [
+  {
+    id: 0,
+    title: "경운이들",
+    content: "경운대학교 재학생 정보 사이트",
+    price: 120000,
+    img: img1
+  },
+  {
+    id: 1,
+    title: "경운대학교 공식 홈페이지",
+    content: "ikw.ac.kr",
+    price: 110000,
+  },
+  {
+    id: 2,
+    title: "LMS",
+    content: "lms.ikw.ac.kr",
+    price: 130000,
+  },
+  {
+    id: 3,
+    title: "CDP",
+    content: "cdp.ikw.ac.kr",
+    price: 130000,
+  },
+  {
+    id: 4,
+    title: "구글",
+    content: "google.com",
+    price: 130000,
+  },
+  {
+    id: 5,
+    title: "네이버",
+    content: "naver.com",
+    price: 130000,
+  },
+];
